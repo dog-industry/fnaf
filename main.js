1669b2dc45eebb0687e33ab58151b87ecf808c4e
dog-industry <91227995+dog-industry@users.noreply.github.com>
2022-01-26 16:36:57 -0600

    1.0.1

--- main.js
+++ main.js
@@ -1,16 +1,62 @@
-function fun1(){
-    document.getElementById("fnaf1").play();
-    console.log("cancion1")
+var canvas=document.getElementById("mi_canvas");
+var ctx=canvas.getContext("2d");
+var color="red"
+var mouseEvent="empty"
+var ultima_posicion_de_x
+var ultima_posicion_de_y
+var ancho_de_linea=1
+canvas.addEventListener("mousedown",mi_mousedown);
+
+function mi_mousedown(l){
+    mouseEvent="mouseDown";
 }
-function fun2(){
-    document.getElementById("fnaf2").play();
-    console.log("cancion2")
+canvas.addEventListener("mousemove",mi_mousemove);
+function mi_mousemove(c){
+    posicion_actual_de_mouse_x=c.clientX-canvas.offsetLeft
+    posicion_actual_de_mouse_y=c.clientY-canvas.offsetTop
+    if(mouseEvent=="mouseDown"){
+        ctx.beginPath();
+        ctx.strokeStyle=color;
+        ctx.lineWidth=ancho_de_linea;
+        ctx.moveTo(ultima_posicion_de_x, ultima_posicion_de_y);
+        ctx.lineTo(posicion_actual_de_mouse_x, posicion_actual_de_mouse_y);
+        ctx.stroke();
+    }
+    ultima_posicion_de_x=posicion_actual_de_mouse_x
+    ultima_posicion_de_y=posicion_actual_de_mouse_y
 }
-function fun3(){
-    document.getElementById("fnaf5").play();
-    console.log("cancion3")
+canvas.addEventListener("mouseup",mi_mouseup);
+function mi_mouseup(l){
+    mouseEvent="mouseUp";
 }
-function fun4(){
-    document.getElementById("fnaf9").play();
-    console.log("cancion4")
+canvas.addEventListener("mouseleave",mi_mouseleave);
+function mi_mouseleave(l){
+    mouseEvent="mouseLeave";
+}
+function borrar(){
+    ctx.clearRect(0,0,ctx.canvas.width,ctx.canvas.height);
+}
+function rojito(){
+    color="red"
+}
+function azulito(){
+    color="blue"
+}
+function verdesito(){
+    color="green"
+}
+function amarillito(){
+    color="yellow"
+}
+function negrito(){
+    color="black"
+}
+function blancito(){
+    color="white"
+}
+function rosita(){
+    color="pink"
+}
+function moradito(){
+    color="purple"
 }
